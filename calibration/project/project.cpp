24235a959624e804bf8502aea846b5e78c799ea1
Dhruv Grover <dhgrover@ucsd.edu>
2014-12-06 00:48:36 -0800

    minor tweaks

--- calibration/project/project.cpp
+++ calibration/project/project.cpp
@@ -36,9 +36,9 @@ std::vector<cv::Point3f> create3DChessboardCorners(cv::Size boardSize, float squ
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	string infilename = "..\\..\\arena\\out_camera_data.xml";
-	string outfilename = "..\\..\\arena\\camera_projection_data.xml";
-	string imgfile = "..\\..\\arena\\cbview.jpg";
+	string infilename = "..\\arena\\out_camera_data.xml";
+	string outfilename = "..\\arena\\camera_projection_data.xml";
+	string imgfile = "..\\arena\\cbview.jpg";
 
 	int boardHeight, boardWidth;
 	float squareSize;
